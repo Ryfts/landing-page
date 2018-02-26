60c5c965f10b3c5015a4c66fced74758add445a8
Guillerming <hello@guillerming.es>
2018-02-26 18:11:28 +0100

    Texts corrections
    Corrections on our vision text.
    Corrections on html markup too (unnesting h4 + compacting code)

--- sections/our-vision.php
+++ sections/our-vision.php
@@ -5,39 +5,21 @@
             <h1 class="section-title">Our mission</h1>
             <div class="row row-eq-height">
                 <div class="col-xs-12 col-sm-12 col-md-8">
-                    <h4 class="align-justify normal-font">
-                        We want to build a decentralized raffle platform where:
-
-                        <br /><br />
-
-                        <h4 class="vision normal-font">
-                            <span class="vision-type-1">Anyone</span> from <span class="vision-type-1">anywhere</span> can <span class="vision-type-1">always</span> play.
-                        </h4>
-
-                        <h4 class="vision normal-font">There are games with the desired <span class="vision-type-2">odds</span>.</h4>
-
-                        <h4 class="vision normal-font"><span class="vision-type-3">Refreshing</span> and <span class="vision-type-3">fun</span> game models are offered.</h4>
-                        <h4 class="vision normal-font">The house can be <span class="vision-type-4">trusted</span> by using cutting edge <span class="vision-type-4">technology</span>.</h4>
-
-                    </h4>
+                    <h4 class="align-justify normal-font">We want to build a decentralized raffle platform where:</h4>
+                    <br />
+                    <h4 class="vision normal-font"><span class="vision-type-1">Anyone</span> from <span class="vision-type-1">anywhere</span> can <span class="vision-type-1">always</span> play.</h4>
+                    <h4 class="vision normal-font">Games provide the players with their desired <span class="vision-type-2">odds</span>.</h4>
+                    <h4 class="vision normal-font"><span class="vision-type-3">Refreshing</span> and <span class="vision-type-3">fun</span> game models are offered.</h4>
+                    <h4 class="vision normal-font">The house can be <span class="vision-type-4">trusted</span> by using cutting edge <span class="vision-type-4">technology</span>.</h4>
                 </div>
 
                 <div class="col-xs-12 col-sm-12 col-md-4">
                     <div class="vision-right-container">
                         <img src="assets/images/home/dices.png" width="200" title="Ryfts dices" class="img-responsive" />
-
                         <br />
-
-                        <p>
-                            We believe that centralized gambling systems are thing of the past and we are one of the pioneers in a new wave.
-                        </p>
-
+                        <p>We believe that centralized gambling systems are a thing of the past and we are one of the pioneers in a new wave</p>
                         <br />
-
-                        <button
-                            class="js-modal-btn button primary-button"
-                            data-video-id="sTLiAcSKrD0"
-                        >Watch&nbsppromo</button>
+                        <button class="js-modal-btn button primary-button" data-video-id="sTLiAcSKrD0">Watch&nbsp;promo</button>
                     </div>
                 </div>
             </div>
